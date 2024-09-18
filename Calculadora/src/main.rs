54a538a4c37c380a6c601c05b0a1abad25f8c8b5
Giovanni Peirone <mgpeirone@escuelasproa.edu.ar>
2024-09-17 21:37:20 -0300

    asd

--- Calculadora/src/main.rs
+++ Calculadora/src/main.rs
@@ -1,9 +1,18 @@
-fn Calcular(n1 : i32, n2: i32, operador: &str ) -> i32
+fn calcular(n1 : i32, n2: i32, operador: &str ) -> i32
 {
-    if (operador == "suma")
-    {
+    if operador == "SUMA" {
         let suma = n1 + n2;
         return suma;
+    }
+    if operador == "RESTA"{
+        let resta = n1 - n2;
+        return resta;
+    }
+    if operador == "MULTI"{
+        let multi = n1 * n2;
+        return multi;
+    }else {
+        return 0;
     }
 }
 
@@ -11,8 +20,8 @@ fn main() {
     let n1 = 4;
     let n2 = 5;
 
-    let suma = Calcular(n1, n2, "suma");
+    let reusltado = calcular(n1, n2, "suma");
 
-    println!("El numero es {suma}");
+    println!("El numero es {reusltado}");
 
 }
